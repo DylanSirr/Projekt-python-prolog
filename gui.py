90d1162d450a35a2401ea9855762831ded4ecb76
Dylan Serrarens <121444972+DylanSirr@users.noreply.github.com>
2025-06-09 22:18:29 +0200

    FINAL, tylko gui trzeba ladniej

--- gui.py
+++ gui.py
@@ -8,32 +8,57 @@ class KierunkiApp:
         master.title("Dopasowanie kierunków studiów")
 
         # Pola wyboru
-        self.zainteresowania = ['programowanie', 'technologie', 'logiczne_myslenie', 'matematyka',
-                                 'fizyka', 'eksperymentowanie', 'czytanie', 'pisanie', 'dyskusje',
-                                 'medycyna', 'ekonomia', 'finanse']
-        self.przedmioty = ['matematyka', 'fizyka', 'chemia', 'biologia', 'filozofia', 'historia', 'geografia']
-        self.cechy = ['logiczne_myslenie', 'kreatywnosc', 'dokladnosc', 'ciekawosc', 'krytyczne_myslenie',
-                      'analiza_danych', 'analityczne_myslenie', 'komunikatywnosc']
-        self.style = ['praktyka', 'samodzielna_nauka', 'analiza_teoretyczna', 'projekty',
-                      'laboratoria', 'czytanie', 'rozmowy']
+        self.zainteresowania = [
+            'programowanie', 'technologie', 'logiczne_myslenie', 'matematyka',
+            'fizyka', 'eksperymentowanie', 'czytanie', 'pisanie', 'dyskusje',
+            'medycyna', 'ekonomia', 'finanse', 'biotechnologia', 'psychologia',
+            'sztuczna_inteligencja', 'robotyka', 'inżynieria', 'prawo',
+            'sztuka', 'muzyka', 'języki_obce', 'socjologia', 'filozofia',
+            'zarządzanie', 'marketing', 'geologia', 'architektura',
+            'gry_komputerowe', 'badania', 'czlowiek', 'nauka', 'innowacje',
+            'zwierzeta', 'budownictwo', 'kosmos', 'analiza'
+        ]
+
+        self.przedmioty = [
+            'matematyka', 'fizyka', 'chemia', 'biologia', 'filozofia', 'historia',
+            'geografia', 'informatyka', 'psychologia', 'ekonomia', 'prawo',
+            'sztuka', 'języki_obce', 'zarządzanie', 'statystyka', 'logika',
+            'elektronika', 'mechanika', 'anatomia', 'socjologia', 'angielski',
+            'wos', 'polski'
+        ]
+
+        self.cechy = [
+            'logiczne_myslenie', 'kreatywnosc', 'dokladnosc', 'ciekawosc',
+            'krytyczne_myslenie', 'analiza_danych', 'analityczne_myslenie',
+            'komunikatywnosc', 'cierpliwosc', 'odpowiedzialnosc', 'wytrwalosc',
+            'zdolnosci_organizacyjne', 'umiejetnosc_pracy_w_zespole',
+            'samodzielnosc', 'adaptacyjnosc', 'empatia', 'otwartosc_na_nowe',
+            'systematycznosc', 'abstrakcyjne_myslenie', 'przestrzenne_myslenie',
+            'obserwacja', 'odpornosc_na_stres', 'elokwencja', 'przedsiebiorczosc'
+        ]
+
+        self.style = [
+            'praktyka', 'samodzielna_nauka', 'analiza_teoretyczna', 'projekty',
+            'laboratoria', 'czytanie', 'rozmowy', 'praca_w_grupie', 'seminaria',
+            'warsztaty', 'prezentacje', 'eksperymenty', 'programowanie',
+            'rozwiązywanie_problemów', 'dyskusje_debaty', 'teoria',
+            'case_studies', 'pamieciowa', 'symulacje', 'obliczenia', 'debata',
+            'analiza'
+        ]
 
         self.check_vars = {}
         self.create_checkboxes()
 
-        # Przycisk
         self.button = tk.Button(master, text="Analizuj", command=self.analizuj)
         self.button.pack(pady=10)
 
-        # Wyniki
-        self.results = tk.Text(master, height=10, width=50)
+        self.results = tk.Text(master, height=15, width=80)
         self.results.pack()
 
-        # Prolog
         self.prolog = Prolog()
         self.prolog.consult("rekomendacje.pl")
 
     def create_checkboxes(self):
-        # Kategorie i checkboxy w siatce
         categories = [('Zainteresowania', self.zainteresowania),
                       ('Przedmioty', self.przedmioty),
                       ('Cechy', self.cechy),
@@ -41,7 +66,7 @@ class KierunkiApp:
         for category_name, items in categories:
             frame = tk.LabelFrame(self.master, text=category_name)
             frame.pack(fill="x", padx=5, pady=5)
-            columns = 4  # ile kolumn checkboxów w jednym wierszu?
+            columns = 8  # Zmieniamy na 8 kolumn
             for index, item in enumerate(items):
                 var = tk.IntVar()
                 cb = tk.Checkbutton(frame, text=item, variable=var)
@@ -51,13 +76,11 @@ class KierunkiApp:
                 self.check_vars[item] = var
 
     def analizuj(self):
-        # Zbieranie wybranych opcji
         zainteresowania = [item for item in self.zainteresowania if self.check_vars[item].get() == 1]
         przedmioty = [item for item in self.przedmioty if self.check_vars[item].get() == 1]
         cechy = [item for item in self.cechy if self.check_vars[item].get() == 1]
         style = [item for item in self.style if self.check_vars[item].get() == 1]
 
-        # Zapytanie do Prologa
         query = f"top5_dopasowania({zainteresowania}, {przedmioty}, {cechy}, {style}, Top5)."
         results = list(self.prolog.query(query, maxresult=1))
 
@@ -66,15 +89,13 @@ class KierunkiApp:
         if results:
             top5 = results[0]['Top5']
             if isinstance(top5, list):
-                if not top5:
-                    top5 = []
                 while len(top5) < 5:
                     top5.append(['Brak_danych', 0])
                 self.results.insert(tk.END, "TOP 5 kierunków:\n\n")
                 for entry in top5:
                     if isinstance(entry, list) and len(entry) == 2:
                         kierunek, wynik = entry
-                        self.results.insert(tk.END, f"{kierunek}: {wynik*10}% dopasowania\n")
+                        self.results.insert(tk.END, f"{kierunek}: {round(wynik*10,1)} punktów\n")
                     else:
                         self.results.insert(tk.END, "Błąd w strukturze danych.\n")
             else:
@@ -84,6 +105,6 @@ class KierunkiApp:
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("700x700")  # kwadratowe okno
+    root.geometry("1400x1000")
     app = KierunkiApp(root)
     root.mainloop()
--- rekomendacje.pl
+++ rekomendacje.pl
@@ -1,97 +1,301 @@
-% --- Fakty ---
-kierunki_studiow('Informatyka', 
-    ['programowanie', 'technologie', 'logiczne_myslenie'], 
-    ['matematyka', 'fizyka'], 
-    ['logiczne_myslenie', 'kreatywnosc'], 
-    ['praktyka', 'samodzielna_nauka']).
+% ---------------------------
+% Fakty: wszystkie kierunki
+% ---------------------------
 
-kierunki_studiow('Matematyka', 
-    ['matematyka', 'logiczne_myslenie'], 
-    ['matematyka', 'fizyka'], 
-    ['logiczne_myslenie', 'dokladnosc'], 
-    ['samodzielna_nauka', 'analiza_teoretyczna']).
+kierunek('Informatyka',
+    [programowanie, technologie, matematyka, gry_komputerowe, elektronika],
+    [matematyka, informatyka, fizyka, angielski],
+    [logiczne_myslenie, kreatywnosc, dokladnosc, cierpliwosc],
+    [praktyka, projekty, samodzielna_nauka]).
 
-kierunki_studiow('Automatyka i Robotyka', 
-    ['technologie', 'programowanie'], 
-    ['fizyka', 'matematyka'], 
-    ['kreatywnosc', 'logiczne_myslenie'], 
-    ['praktyka', 'projekty']).
+kierunek('Inżynieria danych',
+    [analiza_danych, matematyka, statystyka, technologie],
+    [matematyka, informatyka, statystyka, angielski],
+    [analityczne_myslenie, dokladnosc, systematycznosc],
+    [teoria, praktyka, samodzielna_nauka]).
 
-kierunki_studiow('Fizyka Techniczna', 
-    ['fizyka', 'matematyka', 'eksperymentowanie'], 
-    ['fizyka', 'matematyka'], 
-    ['dokladnosc', 'logiczne_myslenie'], 
-    ['samodzielna_nauka', 'laboratoria']).
+kierunek('Matematyka',
+    [matematyka, logika, rozwiazywanie_problemow, badania],
+    [matematyka, fizyka, informatyka],
+    [abstrakcyjne_myslenie, cierpliwosc, dokladnosc],
+    [teoria, samodzielna_nauka, rozwiazywanie_zadan]).
 
-kierunki_studiow('Inżynieria Biomedyczna', 
-    ['technologie', 'medycyna'], 
-    ['biologia', 'chemia', 'matematyka'], 
-    ['kreatywnosc', 'analiza_danych'], 
-    ['praktyka', 'projekty']).
+kierunek('Fizyka',
+    [nauka, badania, kosmos, technologie],
+    [fizyka, matematyka, chemia, informatyka],
+    [ciekawosc, wytrwalosc, analityczne_myslenie],
+    [eksperymenty, teoria, praca_w_grupie]).
 
-% --- Wagi ---
+kierunek('Automatyka i Robotyka',
+    [technologie, programowanie, elektronika, mechanika],
+    [matematyka, fizyka, informatyka],
+    [precyzja, kreatywnosc, logiczne_myslenie],
+    [praktyka, projekty, laboratoria]).
+
+kierunek('Medycyna',
+    [biologia, czlowiek, pomoc, nauka],
+    [biologia, chemia, fizyka, angielski],
+    [odpowiedzialnosc, empatia, odpornosc_na_stres],
+    [pamieciowa, praktyka, praca_w_grupie]).
+
+kierunek('Biotechnologia',
+    [biologia, chemia, badania, innowacje],
+    [biologia, chemia, matematyka, fizyka],
+    [ciekawosc, dokladnosc, cierpliwosc],
+    [laboratoria, projekty, praca_w_grupie]).
+
+kierunek('Weterynaria',
+    [zwierzeta, biologia, pomoc, przyroda],
+    [biologia, chemia, fizyka],
+    [empatia, cierpliwosc, odpornosc_na_stres],
+    [praktyka, pamieciowa, obserwacja]).
+
+kierunek('Ekonomia',
+    [finanse, biznes, analiza, matematyka],
+    [matematyka, geografia, wos, angielski],
+    [analityczne_myslenie, komunikatywnosc, przedsiebiorczosc],
+    [teoria, case_studies, dyskusje]).
+
+kierunek('Finanse i Rachunkowosc',
+    [pieniadze, analiza, biznes, matematyka],
+    [matematyka, wos, angielski],
+    [dokladnosc, systematycznosc, odpowiedzialnosc],
+    [praktyka, case_studies, samodzielna_nauka]).
+
+kierunek('Psychologia',
+    [czlowiek, zachowanie, pomoc, badania],
+    [biologia, wos, angielski],
+    [empatia, cierpliwosc, obserwacja],
+    [teoria, praktyka, dyskusje]).
+
+kierunek('Prawo',
+    [sprawiedliwosc, system, argumentacja, historia],
+    [historia, wos, polski, angielski],
+    [logiczne_myslenie, elokwencja, dokladnosc],
+    [pamieciowa, analiza, debaty]).
+
+kierunek('Inzynieria Ladowa',
+    [budownictwo, projektowanie, infrastruktura, matematyka],
+    [matematyka, fizyka, geografia],
+    [precyzja, przestrzenne_myslenie, odpowiedzialnosc],
+    [projekty, praktyka, obliczenia]).
+
+kierunek('Energetyka',
+    [technologie, ekologia, fizyka, innowacje],
+    [fizyka, matematyka, chemia],
+    [analityczne_myslenie, kreatywnosc, rozwiazywanie_problemow],
+    [laboratoria, projekty, symulacje]).
+
+kierunek('Inzynieria Materialowa',
+    [chemia, fizyka, technologie, badania],
+    [chemia, fizyka, matematyka],
+    [ciekawosc, dokladnosc, cierpliwosc],
+    [eksperymenty, laboratoria, analiza_danych]).
+
+kierunek('Lotnictwo i Kosmonautyka',
+    [technologie, mechanika, fizyka, przestrzen],
+    [fizyka, matematyka, informatyka],
+    [precyzja, logiczne_myslenie, odwage],
+    [symulacje, projekty, praktyka]).
+
+kierunek('Farmacja',
+    [chemia, biologia, medycyna, zdrowie],
+    [chemia, biologia, matematyka],
+    [dokladnosc, cierpliwosc, odpowiedzialnosc],
+    [laboratoria, pamieciowa, praktyka]).
+
+kierunek('Dietetyka',
+    [zdrowie, biologia, czlowiek, zywienie],
+    [biologia, chemia, wos],
+    [empatia, dokladnosc, komunikatywnosc],
+    [praktyka, case_studies, obserwacja]).
+
+kierunek('Ochrona Srodowiska',
+    [przyroda, ekologia, badania, zrownowazony_rozwoj],
+    [biologia, geografia, chemia],
+    [empatia, systematycznosc, krytyczne_myslenie],
+    [praca_w_terenie, projekty, badania]).
+
+kierunek('Biofizyka',
+    [biologia, fizyka, badania, nauka],
+    [fizyka, biologia, matematyka],
+    [analityczne_myslenie, ciekawosc, wytrwalosc],
+    [eksperymenty, teoria, modelowanie]).
+
+kierunek('Rachunkowosc',
+    [finanse, liczby, analiza, biznes],
+    [matematyka, wos, angielski],
+    [dokladnosc, systematycznosc, odpowiedzialnosc],
+    [obliczenia, case_studies, praktyka]).
+
+kierunek('Logistyka',
+    [organizacja, transport, planowanie, biznes],
+    [geografia, matematyka, wos],
+    [analityczne_myslenie, zaradnosc, komunikatywnosc],
+    [case_studies, symulacje, projekty]).
+
+kierunek('Bankowosc i Finanse',
+    [pieniadze, inwestycje, ekonomia, analiza],
+    [matematyka, wos, angielski],
+    [analityczne_myslenie, odpornosc_na_stres, przedsiebiorczosc],
+    [case_studies, symulacje, dyskusje]).
+
+kierunek('Filozofia',
+    [myslenie, etyka, logika, historia],
+    [historia, polski, wos],
+    [krytyczne_myslenie, ciekawosc, elokwencja],
+    [czytanie, dyskusje, eseje]).
+
+kierunek('Historia',
+    [przeszlosc, kultura, badania, spoleczenstwo],
+    [historia, polski, wos],
+    [pamiec, dokladnosc, krytyczne_myslenie],
+    [czytanie, analiza_zrodel, pisanie]).
+
+kierunek('Dziennikarstwo',
+    [media, komunikacja, pisanie, spoleczenstwo],
+    [polski, historia, wos],
+    [komunikatywnosc, ciekawosc, odwage],
+    [wywiady, pisanie, projekty]).
+
+kierunek('Stosunki Miedzynarodowe',
+    [polityka, gospodarka, dyplomacja, kultura],
+    [historia, wos, angielski],
+    [komunikatywnosc, elokwencja, analityczne_myslenie],
+    [dyskusje, case_studies, jezyki]).
+
+kierunek('Grafika',
+    [sztuka, design, technologie, kreatywnosc],
+    [plastyka, informatyka, historia_sztuki],
+    [kreatywnosc, precyzja, wyobraznia],
+    [praktyka, projekty, warsztaty]).
+
+kierunek('Architektura Wnetrz',
+    [design, przestrzen, sztuka, estetyka],
+    [plastyka, historia_sztuki, matematyka],
+    [kreatywnosc, precyzja, przestrzenne_myslenie],
+    [projekty, makietowanie, praktyka]).
+
+kierunek('Fotografia',
+    [sztuka, technologia, obserwacja, kreatywnosc],
+    [plastyka, informatyka, historia_sztuki],
+    [kreatywnosc, cierpliwosc, uwaga_na_szczegoly],
+    [praktyka, projekty, warsztaty]).
+
+kierunek('Filologia Germanska',
+    [jezyki, kultura, literatura, tlumaczenia],
+    [niemiecki, polski, historia],
+    [pamiec, komunikatywnosc, dokladnosc],
+    [czytanie, konwersacje, tlumaczenia]).
+
+kierunek('Filologia Romanska',
+    [jezyki, kultura, literatura, europa],
+    [francuski, polski, historia],
+    [komunikatywnosc, pamiec, elokwencja],
+    [czytanie, dyskusje, pisanie]).
+
+kierunek('Rolnictwo',
+    [przyroda, biologia, technologie, zywienie],
+    [biologia, chemia, geografia],
+    [praktycznosc, cierpliwosc, obserwacja],
+    [praktyka, eksperymenty, praca_w_terenie]).
+
+kierunek('Lesnictwo',
+    [przyroda, ekologia, ochrona_srodowiska, zarzadzanie],
+    [biologia, geografia, chemia],
+    [empatia, odpowiedzialnosc, wytrwalosc],
+    [praca_w_terenie, obserwacja, praktyka]).
+
+kierunek('Kryminologia',
+    [prawo, psychologia, spoleczenstwo, bezpieczenstwo],
+    [wos, historia, biologia],
+    [analityczne_myslenie, obserwacja, odpornosc_na_stres],
+    [case_studies, analiza, praktyka]).
+
+kierunek('Bezpieczenstwo Wewnętrzne',
+    [prawo, polityka, zarzadzanie, spoleczenstwo],
+    [wos, historia, geografia],
+    [odpowiedzialnosc, opanowanie, komunikatywnosc],
+    [case_studies, symulacje, praktyka]).
+
+kierunek('Wychowanie Fizyczne',
+    [sport, zdrowie, ruch, edukacja],
+    [biologia, wf, wos],
+    [aktywnosc, komunikatywnosc, cierpliwosc],
+    [praktyka, warsztaty, trening]).
+
+kierunek('Turystyka i Rekreacja',
+    [podroze, kultura, ruch, przyroda],
+    [geografia, wos, angielski],
+    [komunikatywnosc, zaradnosc, otwartosc],
+    [praktyka, projekty, wycieczki]).
+
+kierunek('Sztuczna Inteligencja',
+    [technologie, programowanie, matematyka, badania],
+    [matematyka, informatyka, fizyka],
+    [logiczne_myslenie, kreatywnosc, abstrakcyjne_myslenie],
+    [algorytmy, projekty, samodzielna_nauka]).
+
+kierunek('Cyberbezpieczenstwo',
+    [technologie, bezpieczenstwo, informatyka, analiza],
+    [informatyka, matematyka, wos],
+    [analityczne_myslenie, dokladnosc, rozwiazywanie_problemow],
+    [praktyka, case_studies, symulacje]).
+
+% ---------------------------
+% Wagi - jeśli chcesz zastosować
+% ---------------------------
 waga(zainteresowania, 3).
 waga(przedmioty, 2).
 waga(cechy, 2).
 waga(style, 1).
 
-% --- Pomocnicze ---
-% Liczenie ile elementów z pierwszej listy występuje w drugiej
+% ---------------------------
+% Pomocnicze predykaty
+% ---------------------------
+
 wspolne_elementy([], _, 0).
-wspolne_elementy([X|Xs], Lista2, Liczba) :-
+wspolne_elementy([X|Xs], Lista2, N) :-
     member(X, Lista2),
-    !,
-    wspolne_elementy(Xs, Lista2, Liczba1),
-    Liczba is Liczba1 + 1.
-wspolne_elementy([_|Xs], Lista2, Liczba) :-
-    wspolne_elementy(Xs, Lista2, Liczba).
+    !, wspolne_elementy(Xs, Lista2, N1),
+    N is N1 + 1.
+wspolne_elementy([_|Xs], Lista2, N) :-
+    wspolne_elementy(Xs, Lista2, N).
 
-% --- Dopasowanie kierunku ---
-dopasowanie(Kierunek, Zainteresowania, Przedmioty, Cechy, Style, Wynik) :-
-    kierunki_studiow(Kierunek, KInt, KPrzedm, KCechy, KStyle),
-    wspolne_elementy(Zainteresowania, KInt, W1),
-    wspolne_elementy(Przedmioty, KPrzedm, W2),
-    wspolne_elementy(Cechy, KCechy, W3),
-    wspolne_elementy(Style, KStyle, W4),
-    waga(zainteresowania, WagaZ),
-    waga(przedmioty, WagaP),
-    waga(cechy, WagaC),
-    waga(style, WagaS),
-    Wynik is W1*WagaZ + W2*WagaP + W3*WagaC + W4*WagaS.
+dopasowanie(K, UZ, UP, UC, US, Wynik) :-
+    kierunek(K, Z, P, C, S),
+    wspolne_elementy(UZ, Z, W1),
+    wspolne_elementy(UP, P, W2),
+    wspolne_elementy(UC, C, W3),
+    wspolne_elementy(US, S, W4),
+    waga(zainteresowania, WZ), waga(przedmioty, WP),
+    waga(cechy, WC), waga(style, WS),
+    WynikValue is W1*WZ + W2*WP + W3*WC + W4*WS,
+    Wynik = [K, WynikValue].
 
-% --- Maksymalne możliwe dopasowanie dla kierunku ---
-maks_dopasowanie(Kierunek, MaxWynik) :-
-    kierunki_studiow(Kierunek, KInt, KPrzedm, KCechy, KStyle),
-    waga(zainteresowania, WagaZ),
-    waga(przedmioty, WagaP),
-    waga(cechy, WagaC),
-    waga(style, WagaS),
-    length(KInt, L1),
-    length(KPrzedm, L2),
-    length(KCechy, L3),
-    length(KStyle, L4),
-    MaxWynik is L1*WagaZ + L2*WagaP + L3*WagaC + L4*WagaS.
+maks_dopasowanie(K, MaxW) :-
+    kierunek(K, Z, P, C, S),
+    waga(zainteresowania, WZ), waga(przedmioty, WP),
+    waga(cechy, WC), waga(style, WS),
+    length(Z, LZ), length(P, LP), length(C, LC), length(S, LS),
+    MaxW is LZ*WZ + LP*WP + LC*WC + LS*WS.
 
-% --- Porównanie do sortowania malejącego ---
 compare_by_score(Delta, [_, P1], [_, P2]) :-
     (P1 > P2 -> Delta = '<' ; Delta = '>').
 
-% --- Pobranie pierwszych N elementów listy ---
 take(0, _, []) :- !.
 take(_, [], []) :- !.
 take(N, [X|Xs], [X|Ys]) :-
     N1 is N - 1,
     take(N1, Xs, Ys).
 
-% --- Główna procedura zwracająca top 5 kierunków z procentami 0-100 ---
-top5_dopasowania(Zainteresowania, Przedmioty, Cechy, Style, Top5) :-
-    findall([Kierunek, Procent],
+top5_dopasowania(UZ, UP, UC, US, Top5) :-
+    findall([K, Procent],
         (
-            kierunki_studiow(Kierunek, _, _, _, _),
-            dopasowanie(Kierunek, Zainteresowania, Przedmioty, Cechy, Style, Wynik),
-            maks_dopasowanie(Kierunek, MaxWynik),
-            MaxWynik > 0,
-            Procent is (Wynik / MaxWynik) * 100
+            dopasowanie(K, UZ, UP, UC, US, [K, W]),
+            maks_dopasowanie(K, M),
+            M > 0,
+            Procent is (W / M) * 100
         ),
         Wyniki),
     predsort(compare_by_score, Wyniki, Posortowane),
